0c9b75cd26c5c2f5569db2805f24151fa6a085d9
Soroka Andrei <javaded78@gmail.com>
2023-04-26 10:30:56 +0300

    init commit

---
+++ src/main/java/com/gmail/javacoded78/latwitter/LAtwitterApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.javacoded78.latwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LAtwitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LAtwitterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/javacoded78/latwitter/LAtwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.javacoded78.latwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LAtwitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
