49b22a86c12fa0909841db118421526e81a22cef
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2023-07-20 20:50:42 +0300

    Added selectors tests.

--- frontend-client/src/store/ducks/user/contracts/state.ts
+++ frontend-client/src/store/ducks/user/contracts/state.ts
@@ -1,34 +1,36 @@
-import {LoadingStatus} from "../../../types";
-import {Image} from "../../../types/common";
-import {BackgroundTheme, ColorScheme} from "../../../../pages/Settings/AccessibilityDisplayLanguages/Display/Display";
-import {AuthUserResponse} from "../../../types/user";
-import {updateGender} from "../actionCreators";
+import { LoadingStatus } from "../../../types";
+import { Image } from "../../../types/common";
+import {
+  BackgroundTheme,
+  ColorScheme,
+} from "../../../../pages/Settings/AccessibilityDisplayLanguages/Display/Display";
+import { AuthUserResponse } from "../../../types/user";
 
 export interface Settings {
-    username?: string;
-    email?: string;
-    countryCode?: string;
-    phone?: number;
-    country?: string;
-    updateGender?: string;
-    language?: string;
-    mutedDirectMessages?: boolean;
-    privateProfile?: boolean;
-    colorScheme?: ColorScheme;
-    backgroundColor?: BackgroundTheme;
+  username?: string;
+  email?: string;
+  countryCode?: string;
+  phone?: number;
+  country?: string;
+  gender?: string;
+  language?: string;
+  mutedDirectMessages?: boolean;
+  privateProfile?: boolean;
+  colorScheme?: ColorScheme;
+  backgroundColor?: BackgroundTheme;
 }
 
 export interface UserRequest {
-    username: string;
-    location: string;
-    website: string;
-    avatar: Image;
-    wallpaper: Image;
-    about: string;
+  username: string;
+  location: string;
+  website: string;
+  avatar: Image;
+  wallpaper: Image;
+  about: string;
 }
 
 export interface UserState {
-    data: AuthUserResponse | undefined;
-    status: LoadingStatus;
+  data: AuthUserResponse | undefined;
+  status: LoadingStatus;
 }
 
