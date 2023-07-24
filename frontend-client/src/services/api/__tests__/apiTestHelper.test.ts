736706110e7431d0141d0ffced9df3187745cf2d
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2023-07-24 11:36:59 +0300

    Added listsApi tests.

--- frontend-client/src/services/api/__tests__/apiTestHelper.test.ts
+++ frontend-client/src/services/api/__tests__/apiTestHelper.test.ts
@@ -3,31 +3,26 @@ import MockAdapter from "axios-mock-adapter";
 
 export const testApiCall = (
     mockAdapter: MockAdapter,
-    method: "get" | "post" | "put" | "delete",
+    method: "onGet" | "onPost" | "onPut" | "onDelete",
     expectedUrl: string,
     statusCode: 200 | 400 | 403 | 404,
     expectedData: any,
-    apiCall: (request?: any) => Promise<AxiosResponse<any>>,
-    requestArgs?: any
+    apiCall: (...request: any) => Promise<AxiosResponse<any>>,
+    ...requestArgs: any[]
 ): void => {
-    let requestHandler;
-
-    if (method === "get") {
-        requestHandler = mockAdapter.onGet(expectedUrl);
-    } else {
-        requestHandler = mockAdapter.onPost(expectedUrl, requestArgs);
-    }
-    requestHandler.reply(statusCode, expectedData);
+    mockAdapter[method](expectedUrl, requestArgs instanceof Object ? requestArgs[0] : requestArgs).reply(statusCode, expectedData);
 
     if (statusCode > 200) {
-        apiCall(requestArgs).then((response) => response)
+        apiCall(...requestArgs)
+            .then((response) => response)
             .catch((error) => {
                 testExpectResponse(error.response, statusCode, expectedUrl, expectedData);
             });
     } else {
-        apiCall(requestArgs).then((response) => {
-            testExpectResponse(response, statusCode, expectedUrl, expectedData);
-        });
+        apiCall(...requestArgs)
+            .then((response) => {
+                testExpectResponse(response, statusCode, expectedUrl, expectedData);
+            });
     }
 };
 
