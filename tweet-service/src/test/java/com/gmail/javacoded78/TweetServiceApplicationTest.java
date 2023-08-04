640eb1e1fe15afbdd9e9ca310bb710f181e20d34
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2023-08-04 09:51:30 +0300

    Added docker file.

--- tweet-service/src/test/java/com/gmail/javacoded78/TweetServiceApplicationTest.java
+++ tweet-service/src/test/java/com/gmail/javacoded78/TweetServiceApplicationTest.java
@@ -1,19 +1,19 @@
-package com.gmail.javacoded78;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-public class TweetServiceApplicationTest {
-
-    @Test
-    void contextLoads() {
-    }
-
-    @Test
-    public void main() {
-        TweetServiceApplication.main(new String[] {});
-    }
-}
+//package com.gmail.javacoded78;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//public class TweetServiceApplicationTest {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//    @Test
+//    public void main() {
+//        TweetServiceApplication.main(new String[] {});
+//    }
+//}
