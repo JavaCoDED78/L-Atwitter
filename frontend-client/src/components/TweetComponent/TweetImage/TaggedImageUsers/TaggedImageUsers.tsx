d35a745ec6d486dc1507e2b9bcf1bf191ab48a7b
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2023-08-08 20:46:15 +0300

    Added description and taggedImageUsers to TweetImage component. Added ImageDescription and TaggedImageUsers components, UserItem component to the TaggedImageUsers component.

---
+++ frontend-client/src/components/TweetComponent/TweetImage/TaggedImageUsers/TaggedImageUsers.tsx
@@ -0,0 +1,84 @@
+import React, { FC, ReactElement } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { List, Typography } from "@material-ui/core";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+import { useTaggedImageUsersStyles } from "./TaggedImageUsersStyles";
+import { getUsersInImage } from "../../../../util/text-formatter";
+import { TaggedUserResponse } from "../../../../types/user";
+import { useModalWindow } from "../../../../hook/useModalWindow";
+import CloseButton from "../../../CloseButton/CloseButton";
+import { fetchTaggedImageUsers, resetTaggedImageUsers } from "../../../../store/ducks/tweet/actionCreators";
+import {
+    selectIsTaggedImageUsersLoading,
+    selectTaggedImageUsers,
+    selectUsersPagesCount
+} from "../../../../store/ducks/tweet/selectors";
+import InfiniteScrollWrapper from "../../../InfiniteScrollWrapper/InfiniteScrollWrapper";
+import Spinner from "../../../Spinner/Spinner";
+import UsersItem, { UserItemSize } from "../../../UsersItem/UsersItem";
+
+interface TaggedImageUsersProps {
+    tweetId: number;
+    taggedImageUsers: TaggedUserResponse[];
+}
+
+const TaggedImageUsers: FC<TaggedImageUsersProps> = ({ tweetId, taggedImageUsers }): ReactElement => {
+    const classes = useTaggedImageUsersStyles();
+    const dispatch = useDispatch();
+    const users = useSelector(selectTaggedImageUsers);
+    const isUsersLoading = useSelector(selectIsTaggedImageUsersLoading);
+    const usersPagesCount = useSelector(selectUsersPagesCount);
+    const { visibleModalWindow, onOpenModalWindow, onCloseModalWindow } = useModalWindow();
+
+    const onClickGetTaggedImageUsers = (): void => {
+        onOpenModalWindow();
+        loadUsers(0);
+    };
+
+    const onCloseTaggedImageUsers = (): void => {
+        onCloseModalWindow();
+        dispatch(resetTaggedImageUsers());
+    };
+
+    const loadUsers = (page: number): void => {
+        dispatch(fetchTaggedImageUsers({ tweetId, pageNumber: page }));
+    };
+
+    return (
+        <>
+            <Typography
+                className={classes.taggedImageUsers}
+                onClick={onClickGetTaggedImageUsers}
+                variant={"subtitle2"}
+                component={"span"}
+            >
+                {getUsersInImage(taggedImageUsers)}
+            </Typography>
+            <Dialog className={classes.dialog} open={visibleModalWindow} onClose={onCloseTaggedImageUsers}>
+                <DialogTitle>
+                    <CloseButton onClose={onCloseTaggedImageUsers} />
+                    In this photo
+                </DialogTitle>
+                <DialogContent id="scrollableDiv" className={classes.content}>
+                    <InfiniteScrollWrapper dataLength={users.length} pagesCount={usersPagesCount} loadItems={loadUsers}>
+                        {isUsersLoading && !users.length ? (
+                            <Spinner paddingTop={250} />
+                        ) : (
+                            <List>
+                                {users.map((user) => (
+                                    <UsersItem key={user.id} user={user} size={UserItemSize.MEDIUM} />
+                                ))}
+                                {isUsersLoading && <Spinner />}
+                            </List>
+                        )}
+                    </InfiniteScrollWrapper>
+                </DialogContent>
+            </Dialog>
+        </>
+    );
+};
+
+export default TaggedImageUsers;
--- frontend-client/src/components/TweetComponent/TweetImage/TweetImage.tsx
+++ frontend-client/src/components/TweetComponent/TweetImage/TweetImage.tsx
@@ -1,58 +1,39 @@
 import React, { FC, memo, ReactElement } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Button, Popover, Typography } from "@material-ui/core";
 
 import { MODAL } from "../../../constants/path-constants";
 import { useTweetImageStyles } from "./TweetImageStyles";
-import { usePopup } from "../../../hook/usePopup";
+import { TaggedUserResponse } from "../../../types/user";
+import ImageDescription from "./ImageDescription/ImageDescription";
+import TaggedImageUsers from "./TaggedImageUsers/TaggedImageUsers";
 
 interface TweetImageProps {
     tweetId?: number;
     imageSrc?: string;
     imageDescription?: string;
+    taggedImageUsers?: TaggedUserResponse[];
 }
 
-const TweetImage: FC<TweetImageProps> = memo(({ tweetId, imageSrc, imageDescription }): ReactElement => {
+const TweetImage: FC<TweetImageProps> = memo((
+    {
+        tweetId,
+        imageSrc,
+        imageDescription,
+        taggedImageUsers
+    }
+): ReactElement => {
     const classes = useTweetImageStyles();
     const location = useLocation();
     const isModal = location.pathname.includes(MODAL);
-    const { popoverId, anchorEl, openPopover, handleOpenPopup, handleClosePopup } = usePopup();
 
     return (
         <div id={"tweetImage"} className={classes.image}>
             <Link to={{ pathname: `${MODAL}/${tweetId}`, state: { background: location } }}>
                 <img className={isModal ? "small" : ""} src={imageSrc} alt={imageSrc} />
             </Link>
-            <div className={classes.altButton} onClick={handleOpenPopup}>
-                ALT
-            </div>
-            <Popover
-                id={popoverId}
-                open={openPopover}
-                anchorEl={anchorEl}
-                onClose={handleClosePopup}
-                classes={{ paper: classes.popover }}
-                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-                transformOrigin={{ vertical: "top", horizontal: "left" }}
-            >
-                <div className={classes.popoverContainer}>
-                    <Typography variant={"h3"} component={"div"}>
-                        Image description
-                    </Typography>
-                    <Typography variant={"subtitle1"} component={"div"}>
-                        {imageDescription}
-                    </Typography>
-                    <Button
-                        onClick={handleClosePopup}
-                        color="primary"
-                        variant="outlined"
-                        size="large"
-                        fullWidth
-                    >
-                        Dismiss
-                    </Button>
-                </div>
-            </Popover>
+            {imageDescription && <ImageDescription imageDescription={imageDescription} />}
+            {(taggedImageUsers && taggedImageUsers.length !== 0) &&
+                <TaggedImageUsers tweetId={tweetId!} taggedImageUsers={taggedImageUsers} />}
         </div>
     );
 });
