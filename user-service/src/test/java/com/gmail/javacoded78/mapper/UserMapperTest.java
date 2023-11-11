ee1a623b4b4741a4cbba4874b7e013b7b325d73a
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2023-11-11 23:48:29 +0300

    Added UserMapper tests.

--- user-service/src/test/java/com/gmail/javacoded78/mapper/UserMapperTest.java
+++ user-service/src/test/java/com/gmail/javacoded78/mapper/UserMapperTest.java
@@ -1,5 +1,7 @@
 package com.gmail.javacoded78.mapper;
 
+import com.gmail.javacoded78.dto.request.SearchTermsRequest;
+import com.gmail.javacoded78.repository.projection.CommonUserProjection;
 import com.gmail.javacoded78.repository.projection.UserProfileProjection;
 import com.gmail.javacoded78.repository.projection.UserProjection;
 import com.gmail.javacoded78.service.UserService;
@@ -11,6 +13,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -45,5 +50,30 @@ public class UserMapperTest extends AbstractAuthTest {
         when(userService.getRelevantUsers()).thenReturn(userProjections.getContent());
         userMapper.getRelevantUsers();
         verify(userService, times(1)).getRelevantUsers();
+    }
+
+    @Test
+    void searchUsersByUsername() {
+        Page<UserProjection> userProjections = UserServiceTestHelper.createUserProjections();
+        when(userService.searchUsersByUsername(TestConstants.USERNAME, pageable, UserProjection.class))
+                .thenReturn(userProjections);
+        userMapper.searchUsersByUsername(TestConstants.USERNAME, pageable);
+        verify(userService, times(1)).searchUsersByUsername(TestConstants.USERNAME, pageable, UserProjection.class);
+    }
+
+    @Test
+    void getSearchResults() {
+        List<CommonUserProjection> commonUserProjectionList = List.of(UserServiceTestHelper.createCommonUserProjection());
+        SearchTermsRequest request = new SearchTermsRequest();
+        when(userService.getSearchResults(request)).thenReturn(commonUserProjectionList);
+        userMapper.getSearchResults(request);
+        verify(userService, times(1)).getSearchResults(request);
+    }
+
+    @Test
+    void startUseTwitter() {
+        when(userService.startUseTwitter()).thenReturn(true);
+        assertTrue(userService.startUseTwitter());
+        verify(userService, times(1)).startUseTwitter();
     }
 }
