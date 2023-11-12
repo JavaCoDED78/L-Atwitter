ce826bedebd3e6f60fdddf8da3ebba4f46baf0a1
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2023-11-12 19:06:21 +0300

    Added UserMapper tests.

--- user-service/src/test/java/com/gmail/javacoded78/mapper/UserMapperTest.java
+++ user-service/src/test/java/com/gmail/javacoded78/mapper/UserMapperTest.java
@@ -1,7 +1,10 @@
 package com.gmail.javacoded78.mapper;
 
 import com.gmail.javacoded78.dto.request.SearchTermsRequest;
+import com.gmail.javacoded78.dto.request.UserRequest;
+import com.gmail.javacoded78.repository.projection.AuthUserProjection;
 import com.gmail.javacoded78.repository.projection.CommonUserProjection;
+import com.gmail.javacoded78.repository.projection.UserDetailProjection;
 import com.gmail.javacoded78.repository.projection.UserProfileProjection;
 import com.gmail.javacoded78.repository.projection.UserProjection;
 import com.gmail.javacoded78.service.UserService;
@@ -15,7 +18,9 @@ import org.springframework.data.domain.Page;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -75,5 +80,38 @@ public class UserMapperTest extends AbstractAuthTest {
         when(userService.startUseTwitter()).thenReturn(true);
         assertTrue(userService.startUseTwitter());
         verify(userService, times(1)).startUseTwitter();
+    }
+
+    @Test
+    void updateUserProfile() {
+        UserRequest userRequest = new UserRequest();
+        userRequest.setFullName(TestConstants.FULL_NAME);
+        userRequest.setAbout(TestConstants.ABOUT);
+        AuthUserProjection authUserProjection = UserServiceTestHelper.createAuthUserProjection();
+        when(userService.updateUserProfile(any())).thenReturn(authUserProjection);
+        userMapper.updateUserProfile(userRequest);
+        verify(userService, times(1)).updateUserProfile(any());
+    }
+
+    @Test
+    void processSubscribeToNotifications() {
+        when(userService.processSubscribeToNotifications(TestConstants.USER_ID)).thenReturn(true);
+        assertTrue(userService.processSubscribeToNotifications(TestConstants.USER_ID));
+        verify(userService, times(1)).processSubscribeToNotifications(TestConstants.USER_ID);
+    }
+
+    @Test
+    public void processPinTweet() {
+        when(userService.processPinTweet(TestConstants.TWEET_ID)).thenReturn(TestConstants.TWEET_ID);
+        assertEquals(TestConstants.TWEET_ID, userService.processPinTweet(TestConstants.TWEET_ID));
+        verify(userService, times(1)).processPinTweet(TestConstants.TWEET_ID);
+    }
+
+    @Test
+    public void getUserDetails() {
+        UserDetailProjection userDetailProjection = UserServiceTestHelper.createUserDetailProjection();
+        when(userService.getUserDetails(TestConstants.USER_ID)).thenReturn(userDetailProjection);
+        userService.getUserDetails(TestConstants.USER_ID);
+        verify(userService, times(1)).getUserDetails(TestConstants.USER_ID);
     }
 }
