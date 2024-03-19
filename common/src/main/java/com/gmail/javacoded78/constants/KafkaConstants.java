16ceab17b23453c9355e6691c67c6a01faf633b8
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-03-19 23:13:28 +0300

    Added kafka producer and consumer to user and topic services.

---
+++ common/src/main/java/com/gmail/javacoded78/constants/KafkaConstants.java
@@ -0,0 +1,8 @@
+package com.gmail.javacoded78.constants;
+
+public class KafkaConstants {
+
+    public static final String UPDATE_USER_TOPIC = "user-service.user.update";
+    public static final String BLOCK_USER_TOPIC = "user-service.user.block";
+    public static final String UNBLOCK_USER_TOPIC = "user-service.user.unblock";
+}
